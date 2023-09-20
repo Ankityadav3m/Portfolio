5c23e385ebdbe68065d62600e6daa1979e8a8a77
ankityadav <ankityadav3m@gmail.com>
2023-09-20 22:05:04 +0530

    first

---
+++ script.js
@@ -0,0 +1,6 @@
+var typed = new Typed('#element', {
+    strings: ['Web Developer', 'Web Designer', 'Graphic Designer', 'UI/UX Designer', 'Front-End Developer'],
+    typeSpeed: 50,
+    loop: true, // Enable looping
+  });
+  
